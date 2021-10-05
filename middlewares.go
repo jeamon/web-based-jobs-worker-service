36d01a2c75313a27098fa195e0cf21472cb21642
Jerome AMON <cloudmentor.scale@gmail.com>
2021-10-05 14:29:22 +0200

    refactored middleware to handle API calls and Web requests

--- middlewares.go
+++ middlewares.go
@@ -1,15 +1,48 @@
 package main
 
-// @middlewares.go contains web requests middleware for logging each URL or API calls.
+// @middlewares.go contains middlewares for decorating each URL or API calls.
 
 import (
+	"context"
 	"net/http"
+	"strings"
 )
 
-// logRequestMiddleware is a middleware that logs incoming request details.
-func logRequestMiddleware(next http.Handler) http.Handler {
+// requestMiddleware adds for each request an id into the request context
+// and logs accordingly its details. For api calls it enables CORS as well.
+func requestMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		weblog.Printf("received request - ip: %s - method: %s - url: %s - browser: %s\n", r.RemoteAddr, r.Method, r.URL.Path, r.UserAgent())
+		// generate and setup request id if not set.
+		requestid := generateID()
+		ctx := context.WithValue(r.Context(), "requestid", requestid)
+		r = r.WithContext(ctx)
+
+		if strings.HasPrefix(r.URL.Path, "/worker/api/") {
+			// api call, enable cors and log.
+			apilog.Printf("[request:%s] - [ip:%s] - [method:%s] - [url:%s] - [agent:%s]\n", requestid, r.RemoteAddr, r.Method, r.URL.Path, r.UserAgent())
+
+			enableCORS(&w, r)
+
+			if r.Method == "OPTIONS" {
+				// responds to pre-flight requests before POST.
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+		} else {
+			// save as web requests.
+			weblog.Printf("request: %s] - [ip: %s] - [method: %s] - [url: %s] - [agent: %s]\n", requestid, r.RemoteAddr, r.Method, r.URL.Path, r.UserAgent())
+		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+// enableCORS enables CORS for API calls.
+func enableCORS(w *http.ResponseWriter, r *http.Request) {
+	if origin := r.Header.Get("Origin"); origin != "" {
+		(*w).Header().Set("Access-Control-Allow-Origin", origin)
+		(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With")
+		(*w).Header().Set("Access-Control-Allow-Credentials", "true")
+	}
 }
